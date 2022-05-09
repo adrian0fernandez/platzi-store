82fec2de8a94041dbb162e16888be46b12b2b9f2
adrian0fernandez <adrianfernandezc01@gmail.com>
2022-05-09 15:37:40 -0500

    preparing project

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { mount } from 'enzyme';
+
+import Footer from "../../components/Footer";
+
+describe('<Footer />', () => {
+    test('Render del componente footer', () => {
+        const footer = mount(<Footer />);
+        expect(footer.legth).toEqual(1);
+    });
+    
+});
